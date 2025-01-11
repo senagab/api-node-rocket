2a45f73ac200b31e6f393a1ab49436d5ecfe5739
senagab <senagab@gmail.com>
2025-01-11 10:12:36 -0400

    feat: configuração inicial fastify e criação de primeira rota

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-import { createServer } from 'node:http'
+// import { createServer } from 'node:http'
 
-const server = createServer((request, response) => {
-    // console.log('oi')
-    response.write('buon giorno')
+// const server = createServer((request, response) => {
+//     // console.log('oi')
+//     response.write('buon giorno')
 
-    return response.end()
+//     return response.end()
+// })
+
+// server.listen(3333)
+
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+server.get('/', () => { // rota: quando o usuario chamar o endereço raiz
+    return 'Hello World'
+}) 
+
+server.get('/123', () => { // rota: quando o usuario chamar o endereço raiz
+    return 'Hello 123'
+}) 
+
+server.get('/pkg', () => { // rota: quando o usuario chamar o endereço raiz
+    return 'This are your packages'
+}) 
+
+server.listen({
+    port: 3333,
 })
-
-server.listen(3333)
