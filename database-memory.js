55a7dec0ba83c655a0c2f7bdc4dbf062703cf67f
senagab <senagab@gmail.com>
2025-01-12 20:40:44 -0400

    feat: endpoints atualização de video com id especificado

--- database-memory.js
+++ database-memory.js
@@ -5,7 +5,16 @@ export class DatabaseMemory {
     #videos = new Map()
 
     list() {
-        return this.#videos.values()
+        // array from => transforma em array
+        return Array.from(this.#videos.entries()).map((videoArray) => {
+            const id = videoArray[0]
+            const data = videoArray[1]
+
+            return {
+                id,
+                ...data,
+            }
+        })
     }
 
     create(video) {
--- server.js
+++ server.js
@@ -6,23 +6,41 @@ const server = fastify()
 const database =new DatabaseMemory()
 
 server.post('/videos', (request, reply) => { // rota para criar video
+    
+    // corpo da requisição
+    const { title, description, duration } = request.body // desestruturação de metadados
+
     database.create({
-        title: "video 01",
-        description: "Este é o video 01",
-        duration: 180,
+        title,
+        description,
+        duration
     })
 
-    console.log(database.list())
+    // console.log(database.list())
 
     return reply.status(201).send()
 }) 
 
 server.get('/videos', () => {
-    return 'Hello 123'
+    const videos = database.list()
+
+    console.log(videos)
+
+    return videos
 }) 
 
-server.put('/videos/:id', () => { // rota para atualizar video
-    return 'This are your packages'
+server.put('/videos/:id', (request, reply) => { // rota para atualizar video
+    const videoId = request.params.id
+    const { title, description, duration } = request.body
+
+    const video = database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send() // http 204 code: resposta de sucesso mas sem conteudo
+
 }) 
 
 server.delete('/videos/:id', () => { // rota para deleção
